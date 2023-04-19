6faeb27e5967dfa134c0adb9ad8ae24af5b29061
Quintus Deacon <quintusdeacon1@gmail.com>
2023-04-19 12:40:52 +0200

    end of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
